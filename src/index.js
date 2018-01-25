bfa8e595358a38c35d6d20eb6e4a3bc8ede32d79
Antonio Calapez <antonio@hackages.io>
2018-01-25 13:50:15 +0100

    Removes CRA boilerplate 😇

--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import {App} from './1-what-is-jsx-anyway/App'
-import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(React.createElement(App), document.getElementById('root'))
-registerServiceWorker()
